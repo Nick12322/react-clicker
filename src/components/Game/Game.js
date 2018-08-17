f2536ed42de2b81efd9ea961e725722635a7dec5
Nick Woodward <nickwoodward5@gmail.com>
2018-08-16 19:15:37 -0700

    added pseudocode, don't know shit

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,16 +5,42 @@ import Card from "../Card";
 import Scoreboard from "../Scoreboard";
 import Footer from "../Footer";
 
+// let chihuahuaArray = ["/assets/images/brown-chihuahua.jpg", "/assets/images/chubby-chihuahua.jpg", "/assets/images/grumpy-chihuahua.jpg", "/assets/images/lady-chihuahua.jpg",  "/assets/images/stern-chihuahua.jpg", "/assets/images/white-chihuahua.jpg"]
+
 class Game extends Component {
 
     state = {
         chihuahuas: cards,
         matches: 0,
         guesses: 0,
-        matchedChihuahua: 3
+        matchedChihuahua: 3,
+        guessed: false
     }
 
+    
+
+    // getRandImg = () => {
+
+        //something to get a random one and set it the image value of the card.
+        // for (let i = 0; i < chihuahuaArray.length; i++)
+        // console.log("From get rand img" + newImg);
+        // return newImg
+    // }
+
+    
+
+    // shuffle = () => {
+        //something to shuffle the image array
+        // this.state.chihuahuas.map(chihuahuaMap => {
+        //     chihuahuaArray.push(chihuahuaMap.image);
+        //     this.getRandImg()
+        // })
+    // }
+
+    
+
     getChihuahua = (id) => {
+        
         if (id === this.state.matchedChihuahua) {
             // if a chihuahua is a match 
             // update the matches state by one 
@@ -23,14 +49,18 @@ class Game extends Component {
             let randomId = Math.floor(Math.random() * this.state.chihuahuas.length) + 1;
             this.setState({
                 matches: this.state.matches + 1,
-                matchedChihuahua: randomId
+                matchedChihuahua: randomId,
             });
+            // this.shuffle();
+            
         } else {
             // if they are not a match update the 
             // guesses state by one 
             this.setState({
-                guesses: this.state.guesses + 1
+                guesses: this.state.guesses + 1,
+                guessed: true
             });
+            // this.shuffle();
         }
     }
 
@@ -44,6 +74,7 @@ class Game extends Component {
                 <div className="row">
                     {this.state.chihuahuas.map(chihuahua => (
                         <Card
+                            guessed={chihuahua.guessed}
                             key={chihuahua.id}
                             id={chihuahua.id}
                             name={chihuahua.name}
